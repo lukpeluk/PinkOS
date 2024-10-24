1c27d903bab1bd75cf46a255c2322c31eee23b3b
Santiago Torres <torres.kilmes@gmail.com>
2024-10-24 17:03:17 -0300

    Refactor docker initialization script

--- .first_time.sh
+++ .first_time.sh
@@ -2,7 +2,6 @@ docker pull agodio/itba-so:2.0
 docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -it --name TPE-Arqui agodio/itba-so:2.0
 
 docker start TPE-Arqui
-docker exec -it TPE-Arqui bash
 
-groupadd -g 1000 -o $USER
-useradd -m -u 1000 -g 1000 -o -s /bin/bash $USER
+docker exec groupadd -g 1000 -o $USER
+docker exec useradd -m -u 1000 -g 1000 -o -s /bin/bash $USER
