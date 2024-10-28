bd3da95b2280ed29cbc5cb7ee21dbd0685271d20
Santiago Torres <torres.kilmes@gmail.com>
2024-10-28 19:46:04 -0300

    Fix docker initialization script

--- .first_time.sh
+++ .first_time.sh
@@ -3,5 +3,5 @@ docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -it --name TPE-A
 
 docker start TPE-Arqui
 
-docker exec groupadd -g 1000 -o $USER
-docker exec useradd -m -u 1000 -g 1000 -o -s /bin/bash $USER
+docker exec TPE-Arqui groupadd -g 1000 -o $USER
+docker exec TPE-Arqui useradd -m -u 1000 -g 1000 -o -s /bin/bash $USER
