6bd5e0b196e7ded4cc0d7009adfd0464fa74ed8e
luk <lmancuso@itba.edu.ar>
2024-10-28 23:53:52 -0300

    notified a known issue in the ./run.sh, that the time on mac is 2 hours behind

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Uncomment for PC
 # qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 
-# Uncomment for Mac
+# Uncomment for Mac (known problem: time is 2 hours behind)
 qemu-system-x86_64 \
   -hda Image/x64BareBonesImage.qcow2 \
   -m 512 \
