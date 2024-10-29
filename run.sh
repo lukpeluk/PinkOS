d55cdfb0745692318195038817b25289e8444e0e
Santiago Torres <torres.kilmes@gmail.com>
2024-10-28 23:59:04 -0300

    Refactor run.sh to support different operating systems

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Uncomment for PC
-# qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
-
-# Uncomment for Mac (known problem: time is 2 hours behind)
-qemu-system-x86_64 \
-  -hda Image/x64BareBonesImage.qcow2 \
-  -m 512 \
-  -audiodev coreaudio,id=speaker \
-  -machine pcspk-audiodev=speaker
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Para Linux
+  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Para Mac
+  qemu-system-x86_64 \
+    -hda Image/x64BareBonesImage.qcow2 \
+    -m 512 \
+    -audiodev coreaudio,id=speaker \
+    -machine pcspk-audiodev=speaker
+else
+  echo "Sistema operativo no soportado"
+  exit 1
+fi
