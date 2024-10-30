e1dc973dfe3b9e4e8aacda076bfb5a48e63d89cd
Santiago Torres <torres.kilmes@gmail.com>
2024-10-30 00:42:00 -0300

    Refactor run.sh to improve readability and maintainability

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Para Linux
-  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+  qemu-system-x86_64 \
+    -hda Image/x64BareBonesImage.qcow2 \
+    -m 512 \
+    -audiodev pa,id=speaker \
+    -machine pcspk-audiodev=speaker
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # Para Mac
   qemu-system-x86_64 \
