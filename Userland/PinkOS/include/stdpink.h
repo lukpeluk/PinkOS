654dd5b6f90076887ab1f3e85fb04d365324b8e1
medinasol <medina.sol.p@gmail.com>
2024-11-02 20:53:42 -0300

    implementación de librería estándar

--- Userland/PinkOS/include/stdpink.h
+++ Userland/PinkOS/include/stdpink.h
@@ -4,7 +4,41 @@
 #include <syscallCodes.h>
 #include <environmentApiEndpoints.h>
 
+/*
+ * Prints a string to the console
+ * @param string the string to print
+ * @return void
+*/ 
 void print(char * string);
 
+/*
+ * Prints a string with the given format to the console 
+ * @param format the format of the string
+ * @param ... the arguments to replace in the format
+ * @return void
+*/
+void printf(char * format, ...);
+
+/*
+ * Prints a single character to the console
+ * @param c the character to print
+ * @return void
+*/
+void putChar(char c);
+
+/*
+ * Reads a character from the console
+ * @return the character read
+*/
+char getChar();
+
+/*
+ * Reads from the console strings
+ * and numbers given a certain format
+ * @param format the format of the string
+ * @param ... pointers to the variables to store the values
+ * @return void
+*/
+void scanf(char * format, ...);
 
 #endif
--- Userland/PinkOS/stdpink.c
+++ Userland/PinkOS/stdpink.c
@@ -1,5 +1,147 @@
 #include <stdpink.h>
+#include <stdarg.h>
+#include <stdin.h>
+
+static char * invalid_format_message = "Invalid format, use %s for a string or %d for a int\n";
 
 void print(char * string){
     syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_STRING_ENDPOINT, string, 0, 0, 0);
+}
+
+//----------------------------------------------------------------------------------------------
+// HELPERS
+//----------------------------------------------------------------------------------------------
+
+// Internal function to convert a number to a string
+char * num_to_string(int num) {
+    static char buffer[12]; //? Cuanto deberia soportar?
+    buffer[11] = '\0';      // Agregar el terminador
+    int i = 10;
+    int is_negative = 0;
+
+    if (num < 0) {
+        is_negative = 1;
+        num = -num;
+    }
+
+    do {
+        buffer[i--] = (num % 10) + '0';
+        num /= 10;
+    } while (num > 0);
+
+    if (is_negative) {
+        buffer[i--] = '-';
+    }
+
+    return &buffer[i + 1];
+}
+
+//----------------------------------------------------------------------------------------------
+// LIBRARY FUNCTIONS
+//----------------------------------------------------------------------------------------------
+
+void strcpy(char * dest, char * src) {
+    while (*src) {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = 0;
+}
+
+void printf(char * format, ...) {
+    va_list args;
+    va_start(args, format);
+    char *str = format;
+
+    while (*str) {
+        if (*str == '%') {
+            str++;
+            switch (*str) {
+                case 'd': {
+                    int num = va_arg(args, int);
+                    syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_STRING_ENDPOINT, num_to_string(num), 0, 0, 0);
+                    break;
+                }
+                case 's': {
+                    char *string = va_arg(args, char *);
+                    syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_STRING_ENDPOINT, string, 0, 0, 0);
+                    break;
+                }
+                default:
+                    syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_STRING_ENDPOINT, invalid_format_message, 0, 0, 0);                
+                    va_end(args);
+                    return;
+            }
+        } else {
+            syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_CHAR_ENDPOINT, *str, 0, 0, 0);
+        }
+        str++;
+    }
+
+    va_end(args);
+}
+
+void putChar(char c){
+    syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_CHAR_ENDPOINT, c, 0, 0, 0);
+}
+
+void puts(char * string){
+    syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_STRING_ENDPOINT, string, 0, 0, 0);
+    syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_CHAR_ENDPOINT, '\n', 0, 0, 0);
+}
+
+char getChar(){
+    return (char)get_char_from_stdin();
+}
+
+//TODO: arreglar el scanf
+void scanf(char * format, ...){
+    va_list args;
+    va_start(args, format);
+    char *str = format;
+
+    while (*str) {
+        if (*str == '%') {
+            str++;
+            switch (*str) {
+                case 'd': {
+                    int *num = va_arg(args, int *);
+                    char c;
+                    int sign = 1;
+                    *num = 0;
+
+                    while ((c = getChar()) != '\n') {
+                        if (c == '-') {
+                            sign = -1;
+                        } else {
+                            *num = *num * 10 + c - '0';
+                        }
+                    }
+
+                    *num *= sign;
+                    break;
+                }
+                case 's': {
+                    char *string = va_arg(args, char *);
+                    char c;
+                    int i = 0;
+
+                    while ((c = getChar()) != '\n') {
+                        string[i++] = c;
+                    }
+
+                    string[i] = 0;
+                    break;
+                }
+                default:
+                    syscall(USER_ENVIRONMENT_API_SYSCALL, PRINT_STRING_ENDPOINT, invalid_format_message, 0, 0, 0);
+                    va_end(args);
+                    return;
+            }
+        }
+        str++;
+    }
+
+    va_end(args);
 }
