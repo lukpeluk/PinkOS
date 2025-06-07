85557ecba1779d7608d38cb117919e3b9c483558
luk <lmancuso@itba.edu.ar>
2025-06-07 17:42:24 -0300

    finalizar los headers de window manager

--- Kernel/include/windowManager/windowManager.h
+++ Kernel/include/windowManager/windowManager.h
@@ -5,20 +5,24 @@
 // una window tiene un pid asociado, un buffer, 
 
 
-/* * getFocusedWindow: 
+/** getFocusedWindow: 
  * Gets the PID of the currently focused window
  * 
  * @return The PID of the focused window, or NULL if no window is present.
 */
 uint32_t getFocusedWindow();
+int isFocusedWindow(uint32_t pid);
 
-/* * getFocusedBuffer: 
+/** getFocusedBuffer: 
  * Gets the buffer of the currently focused window
  * 
  * @return A pointer to the buffer of the focused window, or NULL if no window is present.
 */
 void * getFocusedBuffer();
 
-
+// Agregar y eliminar ventanas, se encarga de allocar y liberar memoria
+// Agregar una ventana devuelve un puntero al buffer de la ventana, o NULL si no se pudo allocar memoria
+void * addWindow(uint32_t pid);
+void removeWindow(uint32_t pid);
 
 
